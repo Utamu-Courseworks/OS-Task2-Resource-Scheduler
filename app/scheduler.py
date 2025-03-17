5b02c366cc968f5725d6e5cbff44e2ae1b74ccd9
RhodaGraice <rhodagraiceobetre@gmail.com>
2025-03-17 11:06:31 +0300

    Added scheduler class and function to generate customers

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,1 +1,24 @@
 #Implements scheduling algorithms (Round Robin, Priority Scheduling, Shortest Job Next).
+
+import time
+import threading
+
+# Scheduler Class
+class Scheduler:
+    def __init__(self, agents):
+        self.agents = agents
+        self.customer_queue = []
+        self.lock = threading.Lock()
+
+    def generate_customers(self):
+        """Simulates random customer arrivals."""
+        while True:
+            customer = Customer(
+                customer_id=random.randint(1000, 9999),
+                service_time=random.randint(3, 10),
+                priority=random.choice(list(PRIORITY_MAP.keys())),
+                arrival_time=time.time()
+            )
+            with self.lock:
+                self.customer_queue.append(customer)
+            time.sleep(random.randint(2, 5))
