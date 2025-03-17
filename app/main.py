dc928bed750b723941c6a563b1a1cfcc25f24bec
gorretgolden <nabatanzigorret143@gmail.com>
2025-03-17 11:09:06 +0300

    added flask cors to reqs

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
 #Runs the scheduling simulation and manages customer-agent interactions.
+
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+# Enabling CORS for all routes
+CORS(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
