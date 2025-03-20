b73b984ea77e43f3d60ad8a5da1e8feaea98cdbf
gorretgolden <nabatanzigorret143@gmail.com>
2025-03-20 12:47:26 +0300

    added round robin and assigning customers to agents functionality

--- models/bank_simulation.py
+++ models/bank_simulation.py
@@ -37,4 +37,32 @@ class BankSimulation:
         for customer in self.queue:
             if available_agents:
                 agent = available_agents.pop(0)
-                threading.Thread(target=self.assign_customer_to_agent, args=(agent, customer), daemon=True).start()        
+                threading.Thread(target=self.assign_customer_to_agent, args=(agent, customer), daemon=True).start()  
+
+   #Function implementation for round robin: fairness scheduling
+    def round_robin_scheduling(self):
+        with self.lock:
+            if not self.queue:
+                return
+            customer = self.queue.pop(0)
+            agent = self.agents[self.agent_index]
+            self.agent_index = (self.agent_index + 1) % len(self.agents)
+            threading.Thread(target=self.assign_customer_to_agent, args=(agent, customer), daemon=True).start()
+
+    #Function to assign agents automatically to customers in the simulation
+    def assign_customer_to_agent(self, agent, customer):
+        with self.lock:
+            agent.status = 'Busy'
+            agent.workload += 1
+            customer.start_service_time = time.time()
+
+        time.sleep(customer.service_time)  # Simulated service time
+
+        with self.lock:
+            customer.served = True
+            customer.wait_time = time.time() - customer.arrival_time  # Calculating wait time when served
+            agent.status = 'Free'
+            agent.workload -= 1
+            agent.customers_served += 1  # Incrementing the number of customers served by the agent
+            agent.total_busy_time += time.time() - customer.start_service_time
+            agent.start_busy_time = None                  
